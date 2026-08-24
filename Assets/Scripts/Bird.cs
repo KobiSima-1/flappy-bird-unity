b7e31051115aa98b1d910fb7a1fa9e781ec663d2
kobi <kobisima3@gmail.com>
2026-08-24 03:17:14 +0300

    Add bird with flap mechanic

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bird : MonoBehaviour
+{
+    [SerializeField] private float flapForce = 7f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Flap();
+        }
+    }
+
+    private void Flap()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, flapForce);
+    }
+}
